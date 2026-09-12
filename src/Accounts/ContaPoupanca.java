623fa384c85ebbd3aec09dc826257773a6dd1ead
Samuel de Almeida <samuel.silva411@outlook.com>
2026-09-12 11:47:31 -0300

    implementar métodos investir e resgatar

--- src/Accounts/ContaPoupanca.java
+++ src/Accounts/ContaPoupanca.java
@@ -1,6 +1,19 @@
 package Accounts;
 
-public class ContaPoupanca extends ContaCorrente {
-    private int saldo;
+import java.util.Scanner;
 
+public class ContaPoupanca extends ContaCorrente {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void investir() {
+        double valorInvestimento;
+        System.out.println("Quanto você quer investir?");
+        valorInvestimento = scanner.nextInt();
+    }
+
+    public void resgatar() {
+        double valorResgate;
+        System.out.println("Quanto você quer resgatar?");
+        valorResgate = scanner.nextInt();
+    }
 }
